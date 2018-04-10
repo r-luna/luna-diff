9329d87821b6c499b6aa14d8b85d1c5b21679b41
Ricardo Luna <rluna@lilly.com>
2018-04-10 10:43:36 -0400

    add more external configs

--- src/js/lunadiff.js
+++ src/js/lunadiff.js
@@ -1,21 +1,23 @@
 ;(function(ns){
 	'use strict';
-	// Adapted from http://rickluna.com/wp/2016/09/diffing-two-canvases/
+    /* global document, Promise, Image, performance */
+	// Adapted from www.rickluna.com/wp/2016/09/diffing-two-canvases/
 
-	var _img1, _img2, _cvs1, _cvs2, _ctx1, _ctx2, _newWidth, _newHeight;
+	var _img1, _img2, _cvs1, _cvs2, _ctx1, _ctx2;
 
-	function _drawOpaqueOverCompareImg(context,alpha){
-		context.globalAlpha = alpha;
-		context.fillStyle = '#ffffff';
-		context.fillRect(0,0,_newWidth,_newHeight);
-	}
+	var _proximity = 13;
+	var _color = '#ff00ff';
+    var _diffThreshold = 5;
+    var _drawBoundingBoxes = false;
+    var _ctx1Alpha = 0.1;
+    var _ctx2Alpha = 0.5;
 	
 	function _loadImage(path){
 		return new Promise(function(resolve,reject){
 			var img = new Image();
 			img.onload = function(){
 				resolve(this);
-			}
+			};
 			img.onerror = function(e){
 				reject(e);
 			};
@@ -23,51 +25,13 @@
 		});
 	}
 	
-	function _setCanvasScale(){
-		return new Promise(function(resolve,reject){
-			// set starting height of canvas
-			_cvs1.height = _cvs2.height = _img1.height;
-			// discover height and width of the image assets. They must both be the same height and width.
-			var img = new Image();
-			
-			// when the image is loaded determine the scale at which they should be rendered into our canvases
-			img.onload = function(){
-				// find the new scale
-				// ratio = width / height;
-				// width = height * ratio;
-				// height = width / ratio;
-				var wrapper = document.getElementById('offscreen');
-				wrapper.appendChild(img);
-				var ratio = img.width / img.height;
-				_newWidth = _cvs1.width;
-				_newHeight = _newWidth / ratio;
-				if (_newHeight > _cvs1.height) {
-					_newHeight = _cvs1.height;
-					_newWidth = _newHeight * ratio;
-				}
-				// clean up
-				wrapper.removeChild(img);
-				// adjust height of the canvases to match the new height value
-				_cvs1.height = _cvs2.height = _newHeight;
-				
-				resolve({success:true});
-			}
-			img.onerror = function(e){
-				reject(e);
-			}
-			// load an image so that we can determine the scale needed to dsiplay them side-by-side
-			img.src = _img1.src;
-		});
-	}
-	
-	
 	function _findDiffAreas(diffs){
 		// Hold the array of diff area arrays, themselves containing coordinate objects
 		var diffAreas = [];
-		var proxPadding = 10;
+		var proxPadding = _proximity;
 		var hit = false;
 		
-		// populate diffAreas with intial diffAreas array and diffs object
+		// populate diffAreas with initial diffAreas array and diffs object
 		diffAreas.push([diffs[0]]);
 		// remove the diffs object that we just added to the above so that it doesnt get added again....
 		diffs.splice(0,1);
@@ -116,7 +80,6 @@
 			// reset
 			hit = false;
 		}
-		//console.log('diffAreas: ',diffAreas);
 		return diffAreas;
 	}
 	
@@ -124,13 +87,13 @@
 		// draw on top of the different pixels
 		for (var i=0;i<diffs.length;i++){
 			//_ctx1.globalCompositeOperation = 'color';
-			_ctx1.globalAlpha = 0.1;
-			_ctx1.fillStyle = '#23cd12';
+			_ctx1.globalAlpha = _ctx1Alpha;
+			_ctx1.fillStyle = _color;//'#23cd12';
 			_ctx1.fillRect(diffs[i].x-1,diffs[i].y-1,2,2);
 			
 			//_ctx2.globalCompositeOperation = 'color';
-			_ctx2.globalAlpha = 0.5;
-			_ctx2.fillStyle = '#ff0000';
+			_ctx2.globalAlpha = _ctx2Alpha;
+			_ctx2.fillStyle = _color;//'#ff0000';
 			_ctx2.fillRect(diffs[i].x-0.5,diffs[i].y-0.5,0.5,0.5);
 		}
 	}
@@ -163,19 +126,19 @@
 			pA2_a = imageData2.data[i+3];
 
 			// compare reds
-			if (pA1_r !== pA2_r){
+			if (Math.abs(pA1_r - pA2_r) > _diffThreshold){
 				diffs.push({x:x,y:y});
 			} else
 			// compare greens
-			if (pA1_g !== pA2_g){
+			if (Math.abs(pA1_g - pA2_g) > _diffThreshold){
 				diffs.push({x:x,y:y});
 			} else
 			// compare blues
-			if (pA1_b !== pA2_b){
+			if (Math.abs(pA1_b - pA2_b) > _diffThreshold){
 				diffs.push({x:x,y:y});
 			} else
 			// compare alphas
-			if (pA1_a !== pA2_a){
+			if (Math.abs(pA1_a - pA2_a) > _diffThreshold){
 				diffs.push({x:x,y:y});
 			}
 		}
@@ -198,7 +161,7 @@
 				}
 			});
 			start.x = diffAreas[j][0].x || 0;
-			end.x   = diffAreas[j][diffAreas[j].length-1].x || w;
+			end.x   = diffAreas[j][diffAreas[j].length-1].x;
 			
 			diffAreas[j].sort(function(a,b){
 				if (a.y < b.y){
@@ -210,7 +173,7 @@
 				}
 			});
 			start.y = diffAreas[j][0].y || 0;
-			end.y   = diffAreas[j][diffAreas[j].length-1].y || h;
+			end.y   = diffAreas[j][diffAreas[j].length-1].y;
 
 			// DONE
 			// start and end have the bounding coordinates
@@ -241,14 +204,33 @@
 		}
 	}
 	
-	ns.init = function(img1path,img2path){
-		var width = window.innerWidth / 2 - 50;
-		_cvs1 = document.getElementById('canvas1');
-		_cvs2 = document.getElementById('canvas2');
+	ns.init = function(img1path,img2path,cb,options){
+        /*
+        var options = 
+            {
+                prox:qwe,
+                color:qwe,
+                diffThreshold:qwe,
+                drawBoundingBoxFlag:qwe,
+                ctx1Alpha:0.5,
+                ctx2Alpha:05
+            };
+		*/
+        var start = performance.now();
+		var end = null;
+		_cvs1 = document.createElement('canvas');
+		_cvs2 = document.createElement('canvas');
 		_ctx1 = _cvs1.getContext('2d');
 		_ctx2 = _cvs2.getContext('2d');
-		_cvs1.width = width; // set canvas widths
-		_cvs2.width = width;
+		_proximity = options.prox || _proximity;
+		_color = options.color || _color;
+        _diffThreshold = options.diffThreshold || _diffThreshold;
+        _drawBoundingBoxes = options.drawBoundingBoxFlag || _drawBoundingBoxes;
+        _ctx1Alpha = options.ctx1Alpha || _ctx1Alpha;
+        _ctx2Alpha = options.ctx2Alpha || _ctx2Alpha;
+		
+		//document.getElementsByTagName('body')[0].appendChild(_cvs1);
+		//document.getElementsByTagName('body')[0].appendChild(_cvs2);
 		
 		// load the images for measurement purposes
 		_loadImage(img1path).then(function(response){
@@ -257,20 +239,76 @@
 		}) //
 		.then(function(response){
 			_img2 = response; // save the loaded image in memory, we will need it later to load into our canvas
-			return _setCanvasScale(); // determine the scaling so that we can proportionately display the canvases
 		})
 		.then(function(){
+			end = performance.now();
+			if (_img1.width !== _img2.width){
+				cb({
+					img1 :{
+						height:_img1.height,
+						width:_img1.width
+					},
+					img2 :{
+						height:_img2.height,
+						width:_img2.width
+					},
+					proximitySetting: _proximity,
+					diffQuan: null,
+					executionTime: (end - start),
+					err: 'images are of unequal width'
+				});
+				return;
+			}
+			
 			var diffs = null;
-			_ctx1.drawImage(_img1,0,0,_newWidth,_newHeight); // insert images into canvases
-			_ctx2.drawImage(_img2,0,0,_newWidth,_newHeight);
+			var diffAreas = null;
+			// set canvas dimensions
+			_cvs1.setAttribute('height',_img1.height);
+			_cvs1.setAttribute('width',_img1.width);
+			_cvs2.setAttribute('height',_img2.height);
+			_cvs2.setAttribute('width',_img2.width);
+			
+			_ctx1.drawImage(_img1,0,0,_img1.width,_img1.height); // insert images into canvases
+			_ctx2.drawImage(_img2,0,0,_img2.width,_img2.height);
+			
 			diffs = _findDiffPixels( // diff the canvases
 				_ctx1.getImageData(0,0,_cvs1.width,_cvs1.height),
-				_ctx2.getImageData(0,0,_cvs2.width,_cvs2.height)
+				_ctx2.getImageData(0,0,_cvs2.width,_cvs2.height),
+                _diffThreshold
 			);
 			
 			_drawOnPixels(diffs); // paint on top of the different pixels
-			//_drawOpaqueOverCompareImg(_ctx2,0.5);
-			_drawDiffAreas(_findDiffAreas(diffs)); // draw a bounding area around the different pixels
+			
+            if (_drawBoundingBoxes){
+			     diffAreas = _findDiffAreas(diffs);
+                _drawDiffAreas(diffAreas); // draw a bounding area around the different pixels
+            }
+			
+			end = performance.now();
+			
+			cb({
+				img1 :{
+					height:_img1.height,
+					width:_img1.width
+				},
+				img2 :{
+					height:_img2.height,
+					width:_img2.width
+				},
+				proximitySetting: _proximity,
+                diffThreshold:_diffThreshold,
+				diffQuan: diffAreas ? diffAreas.length : null,
+				analysisTime: (end - start),
+                isSameDimensions: (_img1.width === _img2.width && _img1.height === _img2.height ? true : false),
+				dimensionDifference: {height:_img1.height - _img2.height,width: _img1.width - _img2.width},
+				err:null,
+                diff1: _cvs1.toDataURL(),
+                diff2: _cvs2.toDataURL()
+			});
+            
+            // reset
+            _img1 = _img2 = _cvs1 = _cvs2 = _ctx1 =  _ctx2 = null;
+			
 		});
 
 	};
